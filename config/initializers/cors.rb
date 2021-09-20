65d8a1c01e4513521496478c4eb9df4800b832de
bettercallazamat <bettercallazamat@gmail.com>
2021-09-21 02:53:24 +0600

    Update cors configs

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins "http://azamats-trips-api.herokuapp.com"
+    origins "https://azamats-trips-api.herokuapp.com"
     resource "*", headers: :any, methods: [:get, :post, :put, :delete, :options, :head],  credentials: true
   end
 end
