bb31218b991a889594db62397f56b5e4e8b33545
bettercallazamat <bettercallazamat@gmail.com>
2021-09-30 02:59:16 +0600

    Edit rack-cors initializers

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
-    resource "*", headers: :any, methods: [:get, :post, :put, :delete, :options, :head],  credentials: true
-  end
-
-  allow do
-    origins "https://azamats-trips-api.herokuapp.com"
-    resource "*", headers: :any, methods: [:get, :post, :put, :delete, :options, :head],  credentials: true
+    origins '*'
+    resource '*', headers: :any, methods: %i[get post]
   end
 end
