71567b7decf807c91c30327b916a9de136dad786
bettercallazamat <bettercallazamat@gmail.com>
2021-09-30 02:25:12 +0600

    Use self generated secret

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -3,11 +3,13 @@ class JsonWebToken
     # def encode(payload, exp = 24.hours.from_now)
     def encode(payload, exp = 10.days.from_now)
       payload[:exp] = exp.to_i
-      JWT.encode(payload, Rails.application.secrets.secret_key_base)
+      # JWT.encode(payload, Rails.application.secrets.secret_key_base)
+      JWT.encode(payload, ENV['TRIPS_SECRET'])
     end
 
     def decode(token)
-      body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+      # body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+      body = JWT.decode(token, ENV['TRIPS_SECRET'])
       HashWithIndifferentAccess.new body
     # rubocop:disable Style/RescueStandardError
     rescue
